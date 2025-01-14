997a1686bf2c01215b66558ea03b2eb041fa9bdf
code-nuage <eloi.hariot@protonmail.com>
2025-01-14 11:04:42 +0100

    0.0.0

--- src/Views/Header.js
+++ src/Views/Header.js
@@ -2,8 +2,8 @@ export default `
 <header id="header">
   <nav class="navbar">
     <div class="container">
-      <a><img src=""></a>
-    </div>
+      <a><img src="" /></a>
+    </div>  
   </nav>
 </header>
 `;
