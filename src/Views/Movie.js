8795313d1af6211b51f76bce2b2ff1ef1e4975a0
code-nuage <eloi.hariot@protonmail.com>
2025-01-15 14:08:18 +0100

    Adds default preview

--- src/Views/Movie.js
+++ src/Views/Movie.js
@@ -52,10 +52,16 @@ const movie = (data) => {
     overview,
     vote_average
   } = data;
+  let preview_path;
+  if (backdrop_path === null) {
+    preview_path = 'https://raw.githubusercontent.com/code-nuage/cinema-roll/refs/heads/main/src/assets/img/no-preview.png';
+  } else {
+    preview_path = `https://image.tmdb.org/t/p/w1280/${backdrop_path}`;
+  }
   /* eslint-enable @typescript-eslint/naming-convention */
   return (`
     <div class='backdrop-path'>
-      <img src='https://image.tmdb.org/t/p/w1280/${backdrop_path}' />
+      <img src="${preview_path}"/>
     </div>
     <div class='infos'>
       <h1>${title}</h1>
